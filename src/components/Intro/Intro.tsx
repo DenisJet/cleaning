d5b1af1627fddb33b4415de66698cfb1df1628f0
Denis Kalkopf <deniskalkopf@gmail.com>
2024-04-10 13:59:26 +0500

    init

---
+++ src/components/Intro/Intro.tsx
@@ -0,0 +1,9 @@
+import styles from './Intro.module.css';
+
+export default function Intro() {
+  return (
+    <section className={styles.intro}>
+      <div className={styles.wrapper}></div>
+    </section>
+  );
+}
