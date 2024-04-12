6f2dce6447d88ea3caab94563f4ce9476dc19830
Denis Kalkopf <deniskalkopf@gmail.com>
2024-04-12 11:15:51 +0500

    fix

--- src/components/Services/Services.tsx
+++ src/components/Services/Services.tsx
@@ -13,26 +13,36 @@ export default function Services({ onClick }: ServicesProps): JSX.Element {
       <ul className={styles.servicesList}>
         <li className={styles.servicesItem}>
           <h3 className={styles.itemTitle}>Уборка домов и квартир</h3>
+          <p className={styles.itemText}>
+            Уборка производится специалистом по чек-листу. Можем выполнить любую услугу на Ваш выбор
+          </p>
           <Button onClick={onClick} />
         </li>
         <li className={styles.servicesItem}>
           <h3 className={styles.itemTitle}>Клининг кафе и ресторанов</h3>
+          <p className={styles.itemText}>
+            Чистота - один из ключевых аспектов успешного развития бизнеса в кафе и ресторанах
+          </p>
           <Button onClick={onClick} />
         </li>
         <li className={styles.servicesItem}>
           <h3 className={styles.itemTitle}>Уборка офисов и производственных помещений</h3>
+          <p className={styles.itemText}>Производим профессиональный клининг коммерческой недвижимости.</p>
           <Button onClick={onClick} />
         </li>
         <li className={styles.servicesItem}>
-          <h3 className={styles.itemTitle}>Химчистка мягкой мебели, ковров, штор...</h3>
+          <h3 className={styles.itemTitle}>Химчистка мягкой мебели, ковров, штор</h3>
+          <p className={styles.itemText}>Знаем всё о том, как поддерживать чистоту мягкой мебели, ковров и штор</p>
           <Button onClick={onClick} />
         </li>
         <li className={styles.servicesItem}>
           <h3 className={styles.itemTitle}>Мойка окон и балконов</h3>
+          <p className={styles.itemText}>Полный спектр услуг по мойке окон и балконов в офисах и квартирах</p>
           <Button onClick={onClick} />
         </li>
         <li className={styles.servicesItem}>
           <h3 className={styles.itemTitle}>Мойка фасадов и витрин</h3>
+          <p className={styles.itemText}>Мойка внешних стеклянных поверхностей: витрин, фасадов, стеллажей</p>
           <Button onClick={onClick} />
         </li>
       </ul>
