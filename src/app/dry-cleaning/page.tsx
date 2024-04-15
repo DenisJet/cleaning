fc59fc88c532a6580c8715311101b2b6c985e7ae
Denis Kalkopf <deniskalkopf@gmail.com>
2024-04-15 10:53:33 +0500

    add dry-cleaning intro

--- src/app/dry-cleaning/page.tsx
+++ src/app/dry-cleaning/page.tsx
@@ -1,24 +1,29 @@
 import Consultation from '@/components/Consultation/Consultation';
 import styles from './page.module.css';
 import Divider from '@/components/Divider/Divider';
+import Button from '@/components/Button/Button';
 
 export default function DryCleaning() {
   return (
     <main className={styles.main}>
       <section className={styles.intro}>
         <div className={styles.container}>
-          <h1>
-            <span>Профессиональная химчистка</span>
-            <br></br>любых предметов интерьера
-          </h1>
-          <p>
-            Профессиональная химчистка мягкой мебели, ковров, штор, портьер, автомобиля! Уборка помещений любой
-            сложности! Обучение!
-          </p>
+          <div className={styles.textContainer}>
+            <h1 className={styles.title}>
+              <span>Профессиональная химчистка</span>
+              <br></br>любых предметов интерьера
+            </h1>
+            <p className={styles.text}>
+              Профессиональная химчистка мягкой мебели, ковров, штор, портьер, автомобиля! Уборка помещений любой
+              сложности! Обучение!
+            </p>
+            <Button />
+          </div>
+          <div className={styles.imageContainer}></div>
         </div>
-        <Divider />
-        <Consultation />
       </section>
+      <Divider />
+      <Consultation />
     </main>
   );
 }
