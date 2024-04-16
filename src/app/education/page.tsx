e96c84574bba7bc47ccae463078bbf321d0838a0
Denis Kalkopf <deniskalkopf@gmail.com>
2024-04-16 11:19:23 +0500

    add education intro section

--- src/app/education/page.tsx
+++ src/app/education/page.tsx
@@ -1,11 +1,32 @@
+'use client';
+import Button from '@/components/Button/Button';
 import styles from './page.module.css';
+import Modal from '@/components/Modal/Modal';
+import Divider from '@/components/Divider/Divider';
 
 export default function Education() {
   return (
-    <main className={styles.main}>
-      <section className={styles.about}>
-        <h1 className={styles.title}>Тут будет Обучение</h1>
-      </section>
-    </main>
+    <>
+      <main className={styles.main}>
+        <section className={styles.intro}>
+          <div className={styles.introContainer}>
+            <div className={styles.textContainer}>
+              <h1 className={styles.introTitle}>
+                <span>Обучение клинингу и химчистке</span>
+                <br></br>от собственников клининговой компании
+              </h1>
+              <p className={styles.introText}>
+                Имеем практические знания и опыт, для того что бы научить и помочь Вам открыть свой успешный бизнес по
+                клинингу, в любом городе России.
+              </p>
+              <Button />
+            </div>
+            <div className={styles.imageContainer}></div>
+          </div>
+        </section>
+        <Divider />
+      </main>
+      <Modal />
+    </>
   );
 }
