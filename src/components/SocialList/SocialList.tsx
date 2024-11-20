a90d4c0f9641647de44c374d881603cab24b9798
Denis Kalkopf <ddenproton@protonmail.com>
2024-11-20 13:17:08 +0500

    change phone

--- src/components/SocialList/SocialList.tsx
+++ src/components/SocialList/SocialList.tsx
@@ -4,17 +4,17 @@ export default function SocialList() {
   return (
     <ul className={styles.list}>
       <li className={styles.item}>
-        <Link className={styles.link} href='https://vk.com/danpower174' target='_blank'>
+        <Link className={styles.link} href='https://vk.com/cleaning.profe' target='_blank'>
           <img className={styles.image} src='icon-vk.svg' alt='иконка соцсети' />
         </Link>
       </li>
       <li>
-        <Link className={styles.link} href='https://t.me/Dan_Power174' target='_blank'>
+        <Link className={styles.link} href='https://t.me/+79995814910' target='_blank'>
           <img className={styles.image} src='icon-tg.svg' alt='иконка соцсети' />
         </Link>
       </li>
       <li>
-        <Link className={styles.link} href='https://wa.me/+79512708525' target='_blank'>
+        <Link className={styles.link} href='https://wa.me/+79995814910' target='_blank'>
           <img className={styles.image} src='icon-wa.svg' alt='иконка соцсети' />
         </Link>
       </li>
