12b1effac6738a1f9f6c0c0ad2f0ad155c316111
Denis Kalkopf <deniskalkopf@gmail.com>
2026-01-23 12:18:45 +0500

    fix form

--- src/components/Form/Form.tsx
+++ src/components/Form/Form.tsx
@@ -4,6 +4,7 @@ import { useRouter } from "next/navigation";
 import { useContext, useRef } from "react";
 import Button from "../Button/Button";
 import { ModalContext } from "@/context/modal.context";
+import axios from "axios";
 
 export default function Form() {
   const { setIsOpen } = useContext(ModalContext);
@@ -13,15 +14,35 @@ export default function Form() {
   const nameRef = useRef<HTMLInputElement>(null);
   const router = useRouter();
 
+  const TOKEN = "7813934402:AAELQTXKWKj4EeniC8DhjIGenzshAF-p084";
+  const CHAT_ID = "-1002400248770";
+  const URI_API = `https://api.telegram.org/bot${TOKEN}/sendMessage`;
+
+  // const TOKEN = "6255542939:AAHERS28Vi18xML8uUg2v1GnJC9OKph6zwg";
+  // const CHAT_ID = "-1001650046621";
+  // const URI_API = `https://api.telegram.org/bot${TOKEN}/sendMessage`;
+
   const handleSubmit = async (evt: { preventDefault: () => void }) => {
     evt.preventDefault();
 
     const name = nameRef.current?.value || "";
     const phone = phoneRef.current?.value || "";
 
+    let message = `<b>Заявка с сайта CleaningProfi</b>\n`;
+    message += `<b>Имя: </b> ${name}\n`;
+    message += `<b>Номер телефона: </b> ${phone}\n`;
+
     try {
+      // Отправляем в Telegram
+      await axios.post(URI_API, {
+        chat_id: CHAT_ID,
+        parse_mode: "html",
+        text: message,
+      });
+
       // Отправляем email через EmailJS
       const templateParams = {
+        to_email: "zakgrom@mail.ru",
         title: "Заявка на обратный звонок с сайта CleaningProfi",
         name: name,
         message: `Номер телефона: ${phone}`,
